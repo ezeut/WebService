b9a5ccdff5d4281ad9abff604d17c5e762638eaf
jg3012 <jg3012@sookmyung.ac.kr>
2021-11-17 01:19:07 +0900

    App.js 레이아웃 구성

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="main-box">
+        <form>
+          <div className="title"> 직업가치관검사 </div>
+          <div className="input-form">
+            <p>
+              이름 <br/>
+              <input type="text" className="name-input"/> 
+            </p>
+            <p>
+              성별 <br/>
+              <label> <input type="radio" className="gender-input" name="gender" value="male" />남자</label> <br/>
+              <label> <input type="radio" className="gender-input" name="gender" value="female" />여자</label> <br/>
+            </p>
+          </div>
+            <button type="submit">검사시작</button>
+        </form>
+      </div>
   );
 }
 
